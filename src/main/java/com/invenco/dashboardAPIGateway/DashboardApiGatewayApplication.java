bb8dfff7c7c163ef74d2f0837d8ec44d3861beb7
Moorthi <Moorthi.Rajan@invenco.com>
2020-09-21 09:55:10 +1200

    Hack-42 Init Repo for Dashboard API Gateway

---
+++ src/main/java/com/invenco/dashboardAPIGateway/DashboardApiGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.invenco.dashboardAPIGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class DashboardApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/invenco/dashboardAPIGateway/DashboardApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invenco.dashboardAPIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
