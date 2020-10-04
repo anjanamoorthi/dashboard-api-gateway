b273ea7512107d0898a0660d7aa58d8ecaad1332
Moorthi <Moorthi.Rajan@invenco.com>
2020-10-04 14:47:12 +1300

    Hack-42 Zuul proxy settings

--- src/main/java/com/invenco/dashboardAPIGateway/DashboardApiGatewayApplication.java
+++ src/main/java/com/invenco/dashboardAPIGateway/DashboardApiGatewayApplication.java
@@ -5,11 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @EnableCircuitBreaker
 @EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
+@EnableSwagger2WebMvc
 public class DashboardApiGatewayApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/invenco/dashboardAPIGateway/config/Swagger2Config.java
@@ -0,0 +1,35 @@
+package com.invenco.dashboardAPIGateway.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Primary
+@Component
+@EnableAutoConfiguration
+public class Swagger2Config implements SwaggerResourcesProvider {
+
+    @Override
+    public List get() {
+        List resources = new ArrayList<>();
+        resources.add(swaggerResource("db-wrapper-service", "/api/release/", "2.0"));
+        resources.add(swaggerResource("db-wrapper-inmemory-service", "/api/release/", "2.0"));
+        resources.add(swaggerResource("db-wrapper-aws-service", "/api/release/", "2.0"));
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location, String version) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
+    }
+
+}
